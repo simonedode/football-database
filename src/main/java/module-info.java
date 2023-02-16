431aba0428d0fb4dc0e57e663456d893e16788d7
Simone Redighieri <simonedode10@gmail.com>
2023-02-16 17:21:40 +0100

    fix: Edited module file to export other directories

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,5 +8,8 @@ module it.unibo.database {
     opens it.unibo.database.model to javafx.base;
     exports it.unibo.database;
     exports it.unibo.database.controller;
+    exports it.unibo.database.model;
     opens it.unibo.database.controller to javafx.fxml;
+    exports it.unibo.database.connection;
+    opens it.unibo.database.connection to javafx.fxml;
 }
