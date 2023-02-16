28c74fdb7c783d009eafb07af490bce9e74ea721
Simone Redighieri <simonedode10@gmail.com>
2023-02-16 20:14:33 +0100

    fix: Added a config file for the database connection

--- src/main/java/it/unibo/database/connection/MySQLConnection.java
+++ src/main/java/it/unibo/database/connection/MySQLConnection.java
@@ -2,13 +2,31 @@ package it.unibo.database.connection;
 
 import it.unibo.database.table.ConnectionProvider;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public final class MySQLConnection {
-    private final static java.sql.Connection CONNECTION = new ConnectionProvider("root", "", "football").getMySQLConnection();
+    private static final java.sql.Connection CONNECTION = new ConnectionProvider(
+            properties().getProperty("DATABASE_USERNAME"),
+            properties().getProperty("DATABASE_PASSWORD"),
+            properties().getProperty("DATABASE_TABLE")
+    ).getMySQLConnection();
 
     private MySQLConnection() {
     }
 
     public static java.sql.Connection getConnection() {
         return CONNECTION;
+    }
+
+    private static Properties properties() {
+        Properties properties = new Properties();
+        String fileName = "app.config";
+        try (FileInputStream fis = new FileInputStream(fileName)) {
+            properties.load(fis);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return properties;
     }
 }
